f1d36b31913b14eee9515df5ab033a0aff20bffd
sharonfe <snuzyfe@yahoo.co.uk>
2016-02-12 23:48:37 +0000

    add, edit, update, show and index listins

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,13 @@ class ArticlesController < ApplicationController
      def new
        @article = Article.new
      end
+     def edit
+      @article = Article.find(params[:id])                                
+     end
+     
+     def index
+        @articles = Article.all                             
+     end
      
      def create
         #render plain: params[:article].inspect
@@ -12,7 +19,19 @@ class ArticlesController < ApplicationController
         else
           render 'new'
         end
-     end                                
+     end   
+     
+     def update
+      @article = Article.find(params[:id])   
+     
+        if @article.update(article_params)
+          flash[:notice] = "Article was successfully updated."
+          redirect_to article_path(@article)
+        else
+          render 'edit'
+        end
+     end
+     
      def show
         @article = Article.find(params[:id])    
      end   
