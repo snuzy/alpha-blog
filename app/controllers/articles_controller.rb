5267564d6ca0721560f80694e4b6303ea5f34ac1
sharonfe <snuzyfe@yahoo.co.uk>
2016-02-13 21:45:21 +0000

    Add patials and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-          flash[:notice] = "Article was created, successfully."
+          flash[:notice] = "Article was successfully created."
           redirect_to article_path(@article)
         else
           render 'new'
@@ -33,8 +33,16 @@ class ArticlesController < ApplicationController
      end
      
      def show
-        @article = Article.find(params[:id])    
+       @article = Article.find(params[:id])    
      end   
+     
+     def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+       flash[:notice] = "Article was successfully deleted!"
+       redirect_to articles_path
+     end
+     
      private
         def article_params
          params.require(:article).permit(:title, :description)                               
